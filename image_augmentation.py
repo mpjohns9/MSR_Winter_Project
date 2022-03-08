dc89b3bc6be6b6775af700b95938737462eacb6c
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-03-08 10:07:54 -0600

    Added functionality for cropped images
    Also added names for other contributors to image dataset

--- image_augmentation.py
+++ image_augmentation.py
@@ -1,55 +1,124 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 import random
 
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 # set which image set is being generated
-test_or_train = 'train'
+test_or_train = 'test'
 
 # list of grip class names
 grip_list = ["changeup", "curveball", "fastball"]
 
+# grip directory
+grip_source = 'cropped'
+num_grips = 10
+
+grayscale = False
+
+resize = False
+
+use_bg = False
+
+name_list = [
+    'marshall',
+    'marshall2',
+    'andru',
+    'anna',
+    'devesh'
+    ]
+
 # iterate through grips and paste onto background image
-for grip in grip_list:
+for name in name_list:
+    for grip in grip_list:
 
-    # grip image
-    grip_img = Image.open(f"images/grips/{grip}_grip.png")
+        grip_img_list = []
 
-    grip_img_list = []
+        for i in range(200):
+            grip_num = random.randint(0, num_grips-1)
 
-    for i in range(1000):
-        grip_x = random.randint(int(grip_img.size[0]*0.6), int(grip_img.size[0]*1.6))
-        grip_y = random.randint(int(grip_img.size[1]*0.6), int(grip_img.size[1]*1.6))
+            # grip image
+            grip_img = Image.open(f"images/grips/{grip_source}/{name}_{grip}_{grip_num}.png")
 
-        grip_resized = grip_img.copy().resize((grip_x, grip_y))
-        grip_img_list.append(grip_resized)
+            if resize:
+                resize_pct = random.uniform(0.5, 0.9)
+            else:
+                resize_pct = 1.0
 
-    count = 1
-    for file in os.listdir(f"images/backgrounds/{test_or_train}"):
+            grip_x = int(grip_img.size[0]*resize_pct)
+            grip_y = int(grip_img.size[1]*resize_pct)
 
-        grip_img = grip_img_list[random.randint(0,999)]
+            grip_resized = grip_img.copy().resize((grip_x, grip_y))
+
+            if random.randint(1,4) == 1:
+                grip_resized = grip_resized.copy().transpose(Image.FLIP_LEFT_RIGHT)
+
+            if random.randint(1,4) == 1:
+
+                rotation = random.randint(-30, 30)
+                if rotation < 0:
+                    rotation = 360 + rotation
+
+                grip_resized = grip_resized.copy().rotate(rotation)
+
+            if not use_bg:
+                filter = ImageEnhance.Brightness(grip_resized)
+                grip_final = filter.enhance(random.uniform(0.75, 1.25))
+                grip_final.save(f"images/grips/augmented/cropped/{test_or_train}/{grip}/{name}_{grip}-{i+1}.jpg")
+                
+            grip_img_list.append(grip_resized)
+
+        if use_bg:
+            bb_dict = {}
+            for i in range(3):
+                count = 1
+                for file in os.listdir(f"images/backgrounds/{test_or_train}"):
+
+                    grip_img = grip_img_list[random.randint(0,999)]
+                    
+                    # background image
+                    bg_img = Image.open(f"images/backgrounds/{test_or_train}/{file}")
+
+                    x_limit = bg_img.size[0] - grip_img.size[0]
+                    y_limit = bg_img.size[1] - grip_img.size[1]
+
+                    if x_limit < 0:
+                        x_limit = 0
+
+                    if y_limit < 0:
+                        y_limit = 0
+
+                    # print(f"GRIP: {grip_img.size}")
+                    # print(f"BG: {bg_img.size}")
+                    # print()
+                    # print(f"XLIM: {x_limit}")
+                    # print(f"YLIM: {y_limit}")
+                    # print()
+
+                    # paste grip onto background and save
+                    x_paste = random.randint(0,x_limit)
+                    y_paste = random.randint(0,y_limit)
+                    bg_img.paste(grip_img, (x_paste, y_paste), grip_img)
+
+                    filter = ImageEnhance.Brightness(bg_img)
+                    bg_img = filter.enhance(random.uniform(0.75, 1.25))
+
+                    bg_img.save(f'images/grips/augmented/{test_or_train}/{grip}/{grip}_{file}-{i}.jpg')
+
+                    key = f"{grip}_{file}-{i}.jpg"
+                    bb_dict[key] = (
+                        x_paste, 
+                        y_paste, 
+                        x_paste + grip_img.size[0], 
+                        y_paste + grip_img.size[1]
+                    )
+
+                    print(count)
+                    count+=1
+            print(f'Finished saving {grip} images.')
+            print()
         
-        # background image
-        bg_img = Image.open(f"images/backgrounds/{test_or_train}/{file}")
-
-        x_limit = bg_img.size[0] - grip_img.size[0]
-        y_limit = bg_img.size[1] - grip_img.size[1]
-
-        if x_limit < 0:
-            x_limit = 0
-
-        if y_limit < 0:
-            y_limit = 0
-
-        # print(f"GRIP: {grip_img.size}")
-        # print(f"BG: {bg_img.size}")
-
-        # paste grip onto background and save
-        bg_img.paste(grip_img, (random.randint(0,x_limit), random.randint(0,y_limit)), grip_img)
-        bg_img.save(f'images/grips/augmented/{test_or_train}/{grip}/{grip}_{file}.jpg')
-        print(count)
-        count+=1
-    print(f'Finished saving {grip} images.')
-    print()
 
 print('All images augmented and saved successfully.')
 
