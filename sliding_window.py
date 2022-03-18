fb628230bd062f9f724a9eaf87c21b4f86a4e842
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-03-18 13:31:35 -0500

    Object detection adjustments

--- sliding_window.py
+++ sliding_window.py
@@ -4,6 +4,7 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 import numpy as np
+import pandas as pd
 
 import classify_grip
 
@@ -30,14 +31,26 @@ transform = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.5,0.5,0.5), std=(0.5,0.5,0.5))])
 
-highest_p = []
-for (x, y, window) in sliding_window(resized):
+highest_p = 0
+top_window = []
+df = pd.DataFrame(columns=[*range((resized.shape[1]-300)//32)])
+row = []
+append_flag = False
+for i, (x, y, window) in enumerate(sliding_window(resized)):
+
     if (x + 300 > resized.shape[1]) or (y + 300 > resized.shape[0]):
+        if append_flag:
+            df.loc[i] = row[:-1]
+        append_flag = False
+        row = []
+        count = 0
         continue
-    cv2.rectangle(resized, (x, y), (x+300, y+300), (255, 0, 0), 2)
-    # cv2.rectangle(resized, (x, y), (x + window[0], y + window[1]), (255, 0, 0), 3)
-    cv2.imshow("test", resized)
-    cv2.waitKey(1)
+
+    append_flag = True
+    # cv2.rectangle(resized, (x, y), (x+300, y+300), (255, 0, 0), 2)
+    # # cv2.rectangle(resized, (x, y), (x + window[0], y + window[1]), (255, 0, 0), 3)
+    # cv2.imshow("test", resized)
+    # cv2.waitKey(1)
     time.sleep(0.05)
     im = Image.fromarray(window*255)
     transformed = transform(im)
@@ -47,7 +60,39 @@ for (x, y, window) in sliding_window(resized):
     prob = torch.softmax(output.data, 1)
     top_p, top_class = prob.topk(1, 1)
 
+    if top_p > highest_p:
+        highest_p = top_p
+        top_window = ((x, y), (x+300, y+300))
+    row.append(top_p.numpy()[0][0])
     
+
+# df.to_csv('output.csv')
+
+df = df.applymap(lambda x: 0.0 if (x < 0.9) else x)
+
+high_score = 0
+high_xy = None
+
+for index in range(df.shape[0]-2):
+    for i in range(df.shape[1]-2):
+        score = df.iloc[index:3+index, i:3+i].values.mean()
+        print(score)
+        if score > high_score:
+            high_score = score
+            high_xy = ((i-1)*32, (index-1)*32)
+        # cv2.rectangle(resized, (i*32, index*32), (364+(i*32), 364+(index*32)), (0, 255, 0), 2)
+        # cv2.imshow("test", resized)
+        # cv2.waitKey(1)
+        # time.sleep(0.05)
+
+# print(high_xy)
+# cv2.rectangle(resized, high_xy, (high_xy[0]+364, high_xy[1]+364), (0, 255, 0), 2)
+# cv2.imshow("test", resized)
+# cv2.waitKey(10000)
+
+classify_window = resized[high_xy[1]:high_xy[1]+428, high_xy[0]:high_xy[0]+428]
+cv2.imwrite('test_image.png', classify_window)
+        
 
 
 
