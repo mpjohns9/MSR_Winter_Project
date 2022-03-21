2c0c80696fddcffc72fdaa435e70462ca55c4c2a
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-03-20 23:48:24 -0500

    Added ability to run prediction function

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+""" This file controls the baseball grip classification package.
+
+Running main.py allows a user to specify arguments to customize
+training a model and running classification in real-time.
+
+To train and save a model run: python3 main.py --train "[SAVED MODEL PATH]"
+The epochs argument allows a user to specify the number of training
+epochs (default is 1000).
+
+To run the prediction file using your laptop webcam, use the argument
+--predict "[SAVED MODEL PATH]"
+
+Direct any questions to marshalljohnson2022@u.northwestern.edu
+"""
+
 import argparse
 import train_model
+from predict import predict_grip
 
 def main():
     """ The main function. """
@@ -8,10 +24,14 @@ def main():
         print("Neither train nor predict arguments declared. Exiting...")
         quit()
 
-    if args.epochs and args.train:
-        network.train(epochs=args.epochs)
-    else:
-        network.train()    
+    if args.train:
+        if args.epochs:
+            network.train(epochs=args.epochs)
+        else:
+            network.train() 
+
+    if args.predict:
+        predict_grip(args.predict)
     
 
 if __name__ == "__main__":
@@ -22,9 +42,3 @@ if __name__ == "__main__":
     parser.add_argument('--predict', type=str, required=False)
     args = parser.parse_args()
     main()
-
-
-    """
-    TASKS:
-
-    """
